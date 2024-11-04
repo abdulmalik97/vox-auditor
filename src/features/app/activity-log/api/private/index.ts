232a615ff39af590f3740fe9bc52ddef63a61e90
Rithvik Arun <43283635+rarun321@users.noreply.github.com>
2024-11-04 10:46:31 -0600

    Added logs

--- src/features/app/activity-log/api/private/index.ts
+++ src/features/app/activity-log/api/private/index.ts
@@ -5,9 +5,10 @@ import axios from "axios";
 export class ActivityLogPrivateApi {
     
   /**
-   * Gets call activity log 
+   * Gets call activity log
    */
   static async getActivityLog() {
+    console.log("Making call with endpoint ", endpoint)
     const response = await axios
       .get( `${endpoint}/get_activity_log`)
       .catch(() => {
@@ -16,6 +17,7 @@ export class ActivityLogPrivateApi {
       });
 
     if (response?.data) {
+      console.log(response?.data)
       return response.data as ActivityLogEntry[];
     } else {
       throw Error("No activity log data returned");
