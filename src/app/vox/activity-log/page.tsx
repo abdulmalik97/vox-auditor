c4ef27fad4cf2744ad713fbba5b17f96dca3db6a
Rithvik Arun <43283635+rarun321@users.noreply.github.com>
2025-01-27 04:14:49 -0700

    UI FIx

--- src/app/vox/activity-log/page.tsx
+++ src/app/vox/activity-log/page.tsx
@@ -2,12 +2,9 @@
 export const dynamic = 'force-dynamic'
 
 import ActivityLogView from "@/features/app/activity-log";
-import { ActivityLogPrivateApi } from "@/features/app/activity-log/api/private";
 
 const ActivityLog = async () => {
-  const activityLogEntries = await ActivityLogPrivateApi.getActivityLog() ?? [];
-
-  return <ActivityLogView activityLogEntries={activityLogEntries} />;
+  return <ActivityLogView />;
 };
 
 export default ActivityLog;
