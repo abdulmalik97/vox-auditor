95cb68313842ff0c0e8af90a12197692e8b48b72
Rithvik Arun <43283635+rarun321@users.noreply.github.com>
2025-03-08 19:27:02 -0600

    Removed unused constant

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,5 @@ export const AZURE_CLIENT_ID = process.env.NEXT_PUBLIC_AZURE_CLIENT_ID
 export const AZURE_CLIENT_SECRET = process.env.NEXT_PUBLIC_AZURE_CLIENT_SECRET
 export const AZURE_TENANT_ID = process.env.NEXT_PUBLIC_AZURE_TENANT_ID
 
-export const LOCAL_BEARER_TOKEN = process.env.NEXT_PUBLIC_LOCAL_BEARER_TOKEN || ''
-
 export const ENV = process.env.NEXT_PUBLIC_ENV
 export const SERVER_ENDPOINT = process.env.NEXT_PUBLIC_SERVER_ENDPOINT
